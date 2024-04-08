7bbf69c4a8fad2ed19423aad207fb35031d200e2
KimNamil <55401148+kimnam1@users.noreply.github.com>
2024-04-09 01:37:10 +0900

    namil homepage start

---
+++ namil_homepage/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { HomeTest } from "../components/home-test";
+
+export default function Home() {
+  return (
+    <div>
+      <HomeTest />
+    </div>
+  );
+}
---
+++ namil_homepage/src/pages/home.tsx
@@ -0,0 +1,9 @@
+import { HomeTest } from "../components/home-test";
+
+export default function HomePage() {
+  return (
+    <div>
+      <HomeTest />
+    </div>
+  );
+}
